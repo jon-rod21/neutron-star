2a82da7816a5c9848faa008e28178e850213efe6
jon-rod21 <jonathan.rodriguez9921@gmail.com>
2025-09-24 18:09:36 -0500

    First commit

---
+++ build/CMakeFiles/neutron.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/neutron.dir/link.d"
+  "CMakeFiles/neutron.dir/libs/glad/src/glad.c.o"
+  "CMakeFiles/neutron.dir/libs/glad/src/glad.c.o.d"
+  "CMakeFiles/neutron.dir/src/main.cpp.o"
+  "CMakeFiles/neutron.dir/src/main.cpp.o.d"
+  "neutron"
+  "neutron.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/neutron.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/neutron.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for neutron.
+# This may be replaced when dependencies are built.
