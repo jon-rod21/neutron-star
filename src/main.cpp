b83a6cb28efadb6fb09faacfb53aecf1d113528c
jon-rod21 <jonathan.rodriguez9921@gmail.com>
2025-09-27 04:44:14 -0500

    began to add vertex calculations for the sphere

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+void genVertices(float radius, int sectors, int stacks);
 
 // SHADER SOURCE: you are telling gpu (in gpu code) how you want to process
 // the vertex information you are giving it
@@ -20,7 +21,7 @@ const char *fragmentShaderSource =
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vec4(0.7f, 0.0f, 0.7f, 1.0f);\n"
     "}\n\0";
 
 int main()
@@ -70,7 +71,8 @@ int main()
     glDeleteShader(fragmentShader);
 
 
-
+    // so this part of the code is where you set up the rendering for drawing
+    // you create your vertex data -> you make your vao, vbo, and maybe ebo
     // VERTEX DATA: here you have the actual points you are drawing onto the screen
     float vertices[] = {
         0.5f, 0.5f, 0.0f,
@@ -78,26 +80,64 @@ int main()
         -0.5f, -0.5f, 0.0f,
         -0.5f, 0.5f, 0.0f
     };
+
+
+
+    std::vector<float>.swap(vertices);
+    std::vector<float>.swap(normals);
+    std::vector<float>.swap(texCoords);
+
+    float x, y, z, xy;
+    float nx, ny, nz, lengthInv = 1.0f / radius;
+    float s, t;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     unsigned int indices[] = {
         0, 1, 3,
         1, 2, 3
     };
     // used by EBO to avoid processing overlapping vertices for larger drawings
 
+    // heres the cool part: your vao is like a book shelf, holding your vbos,
+    // actual data, in memory
+
     unsigned int VBO, VAO, EBO;
+    // this part creates the objects
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
-    glBindVertexArray(VAO);
+    glBindVertexArray(VAO); // VERY IMPORTANT, this activates the vao, ready for vbo's
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // sets VBO as the current active buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
+    // passes the data from vertices to VBO
 
+    // same thing here except with the indices
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+    // tells opengl how to process the data
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(0); // turns on location 0 for the shader 
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -143,3 +183,47 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
+
+void genVertices(float radius, int sectors, int stacks){
+    std::vector<float>.swap(vertices);
+    std::vector<float>.swap(normals);
+    std::vector<float>.swap(texCoords);
+
+    float x, y, z, xy;
+    float nx, ny, nz, lengthInv = 1.0f / radius;
+    float s, t;
+
+
+    float sectorStep = 2 * PI / sectors;
+    float stackStep = PI / stacks;
+    float sectorAngle, stackAngle;
+
+    for (int i = 0; i <= stacks; i++){
+        stackAngle = PI / 2 - i * stackStep;
+        xy = radius * cosf(stackAngle);
+        z = radius * sinf(stackAngle);
+
+        for (int j = 0; j <= sectors; j++){
+            sectorAngle = j * sectorStep;
+
+            x = xy * cosf(sectorAngle);
+            y = xy * sinf(sectorAngle);
+            vertices.push_back(x);
+            vertices.push_back(y);
+            vertices.push_back(z);
+
+            nx = x * lengthInv;
+            ny = y * lengthInv;
+            nz = z * lengthInv;
+            normals.push_back(nx);
+            normals.push_back(ny);
+            normals.push_back(nz);
+            
+            s = (float)j / sectors;
+            t = (float)i / stacks;
+
+            texCoords.push_back(s);
+            texCoords.push_back(t);
+    }
+}
+
